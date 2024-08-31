a1245e52d8070dc31e1cd029ae474a3e1eb37036
vipulwa698 <vipulw78@gmail.com>
2024-09-01 02:17:57 +0530

    Project for Rest Controllers

---
+++ journalApp/src/main/java/com/example/journalApp/Controllers/JournalEntryController.java
@@ -0,0 +1,42 @@
+package com.example.journalApp.Controllers;
+
+import com.example.journalApp.Entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("journal")
+public class JournalEntryController {
+
+    Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAllEntry(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public  boolean createEntry(@RequestBody JournalEntry entries){
+        journalEntries.put(entries.getId(),entries);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getEntryById(@PathVariable long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteEntryById(@PathVariable long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateEntry(@PathVariable long myId, @RequestBody JournalEntry entry){
+        return journalEntries.put(myId, entry);
+    }
+}

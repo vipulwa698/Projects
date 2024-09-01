4513a4f0a7f762dbea263efb2e24b5d078a70a0e
vipulwa698 <vipulw78@gmail.com>
2024-09-01 23:12:41 +0530

    Enabled Lombok

--- journalApp/src/main/java/com/example/journalApp/Entity/JournalEntry.java
+++ journalApp/src/main/java/com/example/journalApp/Entity/JournalEntry.java
@@ -1,12 +1,15 @@
 package com.example.journalApp.Entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Date;
-
+@Getter @Setter
 @Document(collection = "journal_entries")
 public class JournalEntry {
 
@@ -17,35 +20,4 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
